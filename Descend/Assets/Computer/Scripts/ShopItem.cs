df4c601e06acc785641cf91c9dd81498d41d0ea9
Matthew Hyndman <myndman@gmail.com>
2021-04-24 16:58:48 -0400

    Allow purchasing of shop items by clicking on them

--- Descend/Assets/Computer/Scripts/ShopItem.cs
+++ Descend/Assets/Computer/Scripts/ShopItem.cs
@@ -4,24 +4,29 @@ using UnityEngine.UI;
 
 public struct ShopItem
 {
-    public ShopItem(string name, string description, string imageName, int priceInDollars)
+    public ShopItem(string name, string description, string imageName, int priceInDollars, bool isOwned=false)
     {
         this.name = name;
         this.description = description;
         this.imageName = imageName;
         this.priceInDollars = priceInDollars;
+        this.isOwned = isOwned;
     }
     public string name;
     public string description;
     public string imageName;
     public int priceInDollars;
+    public bool isOwned;
 
     public static void RenderToListOfShopItems(ShopItem shopItem, GameObject uiObject)
     {
-        uiObject.transform.Find("Name").GetComponent<Text>().text = shopItem.name;
-        uiObject.transform.Find("Description").GetComponent<Text>().text = shopItem.description;
+        uiObject.GetComponent<Button>().interactable = (!shopItem.isOwned);
+        uiObject.transform.Find("OwnedOverlay").gameObject.SetActive(shopItem.isOwned);
+
+        uiObject.transform.Find("V/Name").GetComponent<Text>().text = shopItem.name;
+        uiObject.transform.Find("V/Description").GetComponent<Text>().text = shopItem.description;
         // TODO: sprite manager to get image. (maybe in the constructor)
-        // uiObject.transform.Find("Image").GetComponent<Image>();
-        uiObject.transform.Find("Price").GetComponent<Text>().text = "$" + shopItem.priceInDollars;
+        // uiObject.transform.Find("V/Image").GetComponent<Image>();
+        uiObject.transform.Find("V/Price").GetComponent<Text>().text = "$" + shopItem.priceInDollars;
     }
 }
--- Descend/Assets/Computer/Scripts/ShopView.cs
+++ Descend/Assets/Computer/Scripts/ShopView.cs
@@ -10,10 +10,26 @@ public class ShopView : MonoBehaviour
     public Text balanceText;
 
     private RenderableGroup<ShopItem> mShopItemsGroup;
+    private List<ShopItem> shopItemList = new List<ShopItem>();
 
     public void Start()
     {
         mShopItemsGroup = new RenderableGroup<ShopItem>(shopItemsContainer, ShopItem.RenderToListOfShopItems);
+        InitializeShopItems();
+    }
+
+    private void InitializeShopItems()
+    {
+        shopItemList.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
+        shopItemList.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
+        shopItemList.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
+        shopItemList.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
+        shopItemList.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
+        shopItemList.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
+        shopItemList.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
+        shopItemList.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
+        shopItemList.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
+        UpdateForSale();
     }
 
     public void Update()
@@ -27,21 +43,15 @@ public class ShopView : MonoBehaviour
 
     public void UpdateForSale()
     {
-        List<ShopItem> results = new List<ShopItem>();
-        results.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
-        results.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
-        results.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
-        results.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
-        results.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
-        results.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
-        results.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
-        results.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
-        results.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
-        mShopItemsGroup.UpdateRenderables(results);
+        mShopItemsGroup.UpdateRenderables(shopItemList);
     }
 
     public void PurchaseItem(ShopItemButton shopItemButton)
     {
+        int itemIndex = shopItemButton.transform.GetSiblingIndex();
+        ShopItem item = shopItemList[itemIndex];
+        PlayerState.Get().DecreaseBalance(item.priceInDollars);
+        shopItemList[itemIndex] = new ShopItem(item.name, item.description, item.imageName, item.priceInDollars, true);
         // TODO: find the corresponding ShopItemButton struct
         // TODO: purchase the corresponding item
     }
