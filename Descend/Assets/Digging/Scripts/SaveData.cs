dd672afb64d8d073f884027b3ce1baced5f657e7
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2021-04-24 13:37:29 -0400

    digging prototype using tilemap

---
+++ Descend/Assets/Digging/Scripts/SaveData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData
+{
+    private static SaveData sSingleton;
+    public static SaveData Get()
+    {
+        if(sSingleton == null)
+        {
+            sSingleton = new SaveData();
+        }
+        return sSingleton;
+    }
+
+    public int digSkill = 0;
+}
---
+++ Descend/Assets/Digging/Scripts/TileManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public List<TileData> tileDatas;
+
+    private Dictionary<TileBase, TileData> tileToData = new Dictionary<TileBase, TileData>();
+
+    private void Awake()
+    {
+        foreach(TileData tileData in tileDatas)
+        {
+            foreach(TileBase tileBase in tileData.tiles)
+            {
+                tileToData.Add(tileBase, tileData);
+            }
+        }
+    }
+
+    public TileData GetTileData(Vector3Int pos)
+    {
+        TileBase tile = tilemap.GetTile(pos);
+        if(tile != null)
+        {
+            return tileToData[tile];
+        }
+
+        return null;
+    }
+}
