1681cb380986f5c9a4d455d6dd210b6a77bcd970
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2021-04-24 15:49:39 -0400

    some procedural generation of the level. good, not great...

--- Descend/Assets/Digging/Scripts/TileData.cs
+++ Descend/Assets/Digging/Scripts/TileData.cs
@@ -9,4 +9,11 @@ public class TileData : ScriptableObject
     public TileBase[] tiles;
 
     public int requiredDigSkill = 0;
+
+    // for proc gen
+    public int introduceDepth;
+    public int backgroundDepth;
+    public int disappearDepthStart;
+    public int disappearDepthEnd;
+    public int maxClusterSize;
 }
