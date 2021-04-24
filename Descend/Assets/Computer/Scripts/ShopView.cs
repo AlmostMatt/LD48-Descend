be9b7ac7afa28062908eb91b24f22de0b29956a5
Matthew Hyndman <myndman@gmail.com>
2021-04-24 15:55:23 -0400

    added content to the shop

--- Descend/Assets/Computer/Scripts/ShopView.cs
+++ Descend/Assets/Computer/Scripts/ShopView.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityBaseCode.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform shopItemsContainer;
+
+    private RenderableGroup<ShopItem> mShopItemsGroup;
+
+    public void Start()
     {
-        
+        mShopItemsGroup = new RenderableGroup<ShopItem>(shopItemsContainer, ShopItem.RenderToListOfShopItems);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        UpdateForSale();
+    }
+
+    public void UpdateForSale()
+    {
+        List<ShopItem> results = new List<ShopItem>();
+        results.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
+        results.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
+        results.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
+        results.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
+        results.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
+        results.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
+        results.Add(new ShopItem("Fancy pickaxe", "Lets you dig 25% faster", "pickaxeImage", 100));
+        results.Add(new ShopItem("Fancier pickaxe", "Lets you dig 50% faster", "pickaxeImage", 300));
+        results.Add(new ShopItem("Fanciest pickaxe", "Lets you dig 100% faster", "pickaxeImage", 500));
+        mShopItemsGroup.UpdateRenderables(results);
     }
 }
