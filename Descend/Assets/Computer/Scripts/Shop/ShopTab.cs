b9c1cb49b523a8a8f139cb9081adf652d663b39c
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2021-04-26 06:33:32 -0400

    show notification on shop tab when an item is available

---
+++ Descend/Assets/Computer/Scripts/Shop/ShopTab.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTab : MonoBehaviour
+{
+    GameObject mNotifications;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mNotifications = transform.Find("Notifications").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SaveData saveData = SaveData.Get();
+        mNotifications.SetActive(saveData.activeShopItem+1 > saveData.digSkill);
+    }
+}
