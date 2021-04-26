3a0cfa3f0ba67c5b9e5ebf7bb1ba0a4853a69706
Matthew Hyndman <myndman@gmail.com>
2021-04-26 01:24:18 -0400

    spawn some rocks at random

--- Descend/Assets/Digging/Scripts/TileDecoration.cs
+++ Descend/Assets/Digging/Scripts/TileDecoration.cs
@@ -2,24 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum DecorationType
+public enum RelativePosition
 {
     TOP,
     SIDE,
     BOTTOM
 }
 
+public enum CreateTiming
+{
+    START_ONLY,
+    AFTER_DESTROY_ONLY,
+    EITHER,
+}
+
 public class TileDecoration : MonoBehaviour
 {
     // TODO: figure out if decorations should have a chance or be guaranteed etc
     // ALSO, check if top layer (grass) should be special cased
+    // TODO: indicate if toppers are initial-only or if they can be left behind when other tiles are destroyed
+    // check if "one of x" (for example grass + rock is ok)
 
+    // I want to guarantee that grass spawns at start and does not spawn later
 
-    public DecorationType decorationType = DecorationType.TOP;
-
-    public void Start()
-    {
-        // if top or bottom, randomly reflect
-        // if left vs right, reflect
-    }
+    public RelativePosition relativePos = RelativePosition.TOP;
+    public CreateTiming createTiming = CreateTiming.EITHER;
+    public float probability = 0.1f;
 }
